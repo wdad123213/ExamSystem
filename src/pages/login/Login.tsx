42478ee1f284efa290e198908726414961fb25e8
马璟 <2443065482@qq.com>
2024-08-12 18:09:52 +0800

    登录

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -37,7 +37,7 @@ const Login:React.FC = () => {
 
   };
   const getCode = async()=>{
-    const res = await captchaApi()
+    const res = await captchaApi() 
     // console.log(res)
     setImg(res.data.data.code)
   }
