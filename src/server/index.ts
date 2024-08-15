c3dc790708efc368920dc45af7e554bd1e8c87c2
吴江 <2731105140@qq.com>
2024-08-15 12:06:41 +0800

    aa

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,9 @@
 import request from './request.tsx'
 import {
   LoginResponse,
-  LoginParams
+  LoginParams,
+  classParams,
+  studentParams
 } from "../types/api"
 
 export const loginApi = (params: LoginParams) => {
@@ -10,6 +12,46 @@ export const loginApi = (params: LoginParams) => {
 
 export const captchaApi = () => {
   return request.get('/login/captcha')
+}
+
+export const questionList = () => {
+  return request.get('/question/list')
+}
+//考试列表
+export const examListApi = ()=>{
+  return request.get('/examination/list?creator=root')
+}
+// 删除考试
+export const removeExamListApi = ()=>{
+  return request.get('/examination/remove')
+}
+
+export const classListApi = (params: classParams) => {
+  return request.get('/studentGroup/list',{...params})
+}
+
+export const classDelApi = (id: any) => {
+  return request.post(`/studentGroup/remove`,{id})
+}
+
+export const classSaveApi = (id: any,it: any) => {
+  return request.post('/studentGroup/update',{id,...it})
+}
+
+export const studentListApi = (params: studentParams) => {
+  return request.get('/student/list',{...params})
+}
+
+export const studentDelApi = (id: any) => {
+  return request.post(`/student/remove`,{id})
+}
+
+export const studentSaveApi = (id: any,it: any) => {
+  return request.post('/student/update',{id,...it})
+}
+
+export const userInfoApi = () => {
+  return request.get('/user/info')
 }
 
 export const questionList = () => {
---
+++ src/types/api/index.ts
@@ -0,0 +1,67 @@
+export interface BaseResponse {
+  code: number;
+  msg: string;
+}
+
+export interface BaseValuse<T> {
+  code: number;
+  msg: string;
+  values: T
+}
+
+
+type Token = {
+  token: string
+}
+
+// 登录
+export type LoginParams = Record<'username' | 'password'|'code', string>
+export type LoginResponse = BaseResponse & {
+  data: Token
+}
+
+
+export interface DataType {
+  key?: string;
+  classify?: string
+  createTime?: string
+  creator?: string
+  endTime?: string
+  examId?: string
+  examiner?: string[]
+  group?: string[]
+  name?: string
+  questionsList?: string[]
+  startTime?:  String
+  _id?: string
+  status?: number
+}
+// 用户信息
+export type userInfo = {
+  _id: string,
+  username: string,
+  password: string,
+  status: number
+  __v: number
+  avator?: string
+}
+
+export type classParams = {
+  page: String,
+  pagesize: String,
+  name: String,
+  teacher: String,
+  classify: String,
+  
+}
+
+export type studentParams = {
+  username: String,
+  sex: String,
+  age: String,
+  className: String,
+  pageSize: String,
+  current: String,
+  // page: String,
+  
+}
